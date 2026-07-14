89824006f7d382e3a320526c85835d9537505c48
Manikandaprabu <65881102+mkpg@users.noreply.github.com>
2026-07-14 14:19:20 +0530

    Create 43-MultiplyStrings

---
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -0,0 +1,8 @@
+# Last updated: 7/14/2026, 2:19:15 PM
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        num1 = int(num1)
+        num2 = int(num2)
+        num3 = num1*num2
+        # num3: str
+        return str(num3)
