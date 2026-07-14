e996eea4ea5e838dc3fd72b778df53a3fbe38f16
Manikandaprabu <65881102+mkpg@users.noreply.github.com>
2026-07-14 14:17:54 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,20 @@
+// Last updated: 7/14/2026, 2:17:49 PM
+import java.util.*;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if (a.length != b.length) {
+            return false;
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
