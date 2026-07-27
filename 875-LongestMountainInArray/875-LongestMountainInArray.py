8b0e66bde2d98be4c7f36e68d20bf0a81829793c
Manikandaprabu <65881102+mkpg@users.noreply.github.com>
2026-07-27 11:56:39 +0530

    Create 875-LongestMountainInArray

---
+++ 875-LongestMountainInArray/875-LongestMountainInArray.py
@@ -0,0 +1,20 @@
+# Last updated: 7/27/2026, 11:56:36 AM
+class Solution:
+    def longestMountain(self, arr: List[int]) -> int:
+        n = len(arr)
+        ans = 0
+
+        for i in range(1, n - 1):
+            if arr[i - 1] < arr[i] > arr[i + 1]:
+                l = i
+                r = i
+
+                while l > 0 and arr[l - 1] < arr[l]:
+                    l -= 1
+
+                while r < n - 1 and arr[r] > arr[r + 1]:
+                    r += 1
+
+                ans = max(ans, r - l + 1)
+
+        return ans
